9becd5b939e6819ddd59b8cadde05749cc444662
lvzhaohu <zhaohulv99999@163.com>
2020-05-04 13:37:18 +0800

    班级实体类

---
+++ class_management_system/src/main/java/bean/ClassInfoBean.java
@@ -0,0 +1,29 @@
+package bean;
+
+
+public class ClassInfoBean {
+
+    private Integer cid;
+
+    private String cname;
+
+    public ClassInfoBean() {
+
+    }
+
+    public Integer getCid() {
+        return cid;
+    }
+
+    public void setCid(Integer cid) {
+        this.cid = cid;
+    }
+
+    public String getCname() {
+        return cname;
+    }
+
+    public void setCname(String cname) {
+        this.cname = cname;
+    }
+}
